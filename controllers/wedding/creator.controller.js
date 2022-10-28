d02f4d0512ab1189cbd09898b116a4b951cad1fc
dineshdevelops <dineshdevelops@gmail.com>
2022-10-28 21:42:20 +0530

    fix: Wedding Invitation Approve API

--- controllers/wedding/creator.controller.js
+++ controllers/wedding/creator.controller.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const WeddingCreator = require("../../models/wedding/WeddingCreator.model")
-
+const {sendWeddingInvitationApproveEmail} = require("../../controllers/authentication/nodemail.controller")
 const postCreatorPage = asyncHandler(async(req,res)=>{
     const weddingRes = await WeddingCreator.create(req.body);
     console.log(weddingRes)
     res.status(200).json({message:"Successfully Created your Invitation wait for approval",weddingRes})
 })
 
-module.exports = {postCreatorPage}
+const approveWeddingInvitation = asyncHandler(async(req,res)=>{
+    const {invitationId,userId,emailId}=req.body;
+    const weddingCreatorRes = await WeddingCreator.updateOne({_id:invitationId , isApproved:true})
+    const invitationUrl = `http://localhost:${process.env.UI_PORT}/wedding/${invitationId}`
+    const emailRes = await sendWeddingInvitationApproveEmail(emailId,invitationUrl)
+    res.status(200).json({message:"Wedding Invitation has be Approved"})
+})
+module.exports = {postCreatorPage,approveWeddingInvitation}
--- routes/wedding.routes.js
+++ routes/wedding.routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {postCreatorPage} = require("../controllers/wedding/creator.controller")
+const {postCreatorPage,approveWeddingInvitation} = require("../controllers/wedding/creator.controller")
 const {getWeddingInvitation} = require("../controllers/wedding/invitation.controller")
 
 router.post("/postCreator",postCreatorPage);
 router.get("/getWeddingInvitation",getWeddingInvitation);
+
+//! Wedding Invitation API for ALIKA admin
+router.post('/approveInvitation',approveWeddingInvitation)
 
 module.exports = router;
