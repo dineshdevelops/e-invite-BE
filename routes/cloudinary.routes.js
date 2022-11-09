55e0f09194f66d53de82902f1545f91b8e2e179a
dineshdevelops <dineshdevelops@gmail.com>
2022-11-09 21:26:00 +0530

    fix: delete Image API

--- routes/cloudinary.routes.js
+++ routes/cloudinary.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const {uploadImage,deleteImage} = require("../controllers/cloudinary/cloudinary.controller")
 
 router.post("/uploadImage",uploadImage);
-router.delete("/deleteImage",deleteImage);
+router.post("/deleteImage",deleteImage);
 
 module.exports=router;
